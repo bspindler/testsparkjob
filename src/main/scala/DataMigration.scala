98d635db70c9a1eb05d0fdcd80d5a7aef82a6879
bspindler <brian.spindler@gmail.com>
2017-06-19 12:10:22 -0400

    remove license header

--- src/main/scala/DataMigration.scala
+++ src/main/scala/DataMigration.scala
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) ${year} ${name} <${email}> All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Netuitive, Inc. and certain third parties ("Confidential Information").
- * You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement
- * you entered into with Netuitive.
- *
- * NETUITIVE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- * OR NON-INFRINGEMENT. NETUITIVE SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
- */
-
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.writer.{TTLOption, WriteConf}
